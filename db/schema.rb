e3edc77c368ba305a8ab2dd1212dcdfa21865dc9
ZakharDay <zakhar.day@gmail.com>
2025-10-26 22:20:21 +0300

    Work snapshot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_19_141853) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_26_140132) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_19_141853) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.string "cover"
   end
 
   add_foreign_key "comments", "posts"
